0e36728c468713de5ad95d676bf103426906465b
drop-shop <office.dropshop@gmail.com>
2016-10-30 13:12:23 +0200

    add site files

---
+++ js/common.js
@@ -0,0 +1,43 @@
+$(function() {
+
+
+  $(document).ready(function() {
+
+  // full page
+  $('.site-content').fullpage({
+   menu : '.site-menu',
+   anchors:['home', 'about', 'portfolio' ],
+   verticalCentered:false
+ });
+
+  // slider
+  var slider = $('.portfolio-slider')
+  slider.owlCarousel({
+      nav : false, // Show next and prev buttons
+      dots: false,
+      singleItem: true,
+      afterAction : reloadNum
+    });
+
+  function reloadNum(event, info){
+    var curentItem = this.owl.currentItem + 1;
+    var itemLegth = this.owl.owlItems.length;
+    var result = curentItem + ' / ' + itemLegth;
+    $('.slider-controls').find('.slider-number').text(result);
+  }
+
+  $(".next").click(function(){
+    slider.trigger('owl.next');
+  })
+
+  $(".prev").click(function(){
+    slider.trigger('owl.prev');
+  })
+
+  // tabs
+  $('.about-tabs').tabslet();
+
+
+});
+
+});
